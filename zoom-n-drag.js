f93b0d6c717c5776e70246c6276aae914f1dfc62
Ronei Kunkel <ronei.kunkel@gmail.com>
2024-03-28 00:24:50 -0300

    refactor

---
+++ zoom-n-drag.js
@@ -0,0 +1,64 @@
+// Seleciona o contêiner do SVG
+const svgWrapper = document.getElementById('svg-wrapper');
+
+// Variáveis para controlar o movimento do SVG
+let isDragging = false;
+let startX, startY, translateX = 0, translateY = 0;
+let scale = 1;
+
+// Adiciona um ouvinte de evento para pressionar o mouse
+svgWrapper.addEventListener('mousedown', function (event) {
+  event.preventDefault();
+
+  // Atualiza o estado do movimento para true
+  isDragging = true;
+
+  startX = event.clientX;
+  startY = event.clientY;
+
+  svgWrapper.classList.add('grabbing');
+});
+
+// Adiciona um ouvinte de evento para soltar o mouse
+window.addEventListener('mouseup', function () {
+  isDragging = false;
+  svgWrapper.classList.remove('grabbing');
+});
+
+// Adiciona um ouvinte de evento para mover o mouse
+window.addEventListener('mousemove', function (event) {
+  if (!isDragging) return;
+
+  // Calcula a distância percorrida pelo mouse, dividindo pelo fator de escala
+  const deltaX = event.clientX - startX;
+  const deltaY = event.clientY - startY;
+
+  // Atualiza as posições de translação do SVG
+  translateX += deltaX;
+  translateY += deltaY;
+
+  // Aplica a transformação de translação e escala ao SVG
+  svgWrapper.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`;
+
+  // Atualiza as coordenadas iniciais do mouse
+  startX = event.clientX;
+  startY = event.clientY;
+});
+
+// Adiciona um ouvinte de evento para a roda do mouse (zoom)
+svgWrapper.addEventListener('wheel', function (event) {
+  event.preventDefault();
+
+  const direction = event.deltaY < 0 ? 1 : -1;
+
+  const zoomFactor = 1.1;
+
+  scale += direction === 1 ? 1 : -1;
+
+  // Limita o zoom mínimo e máximo
+  if (scale < 0.1) scale = 0.1;
+  if (scale > 3) scale = 3;
+
+  // Aplica a transformação de translação e escala ao SVG
+  svgWrapper.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`;
+});
