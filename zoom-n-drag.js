854bc2d419d4868c05214841b1c266e722c9dfda
pablovas <pablo71042001@gmail.com>
2024-04-14 17:17:57 -0300

    Final adjusts

--- zoom-n-drag.js
+++ zoom-n-drag.js
@@ -1,12 +1,14 @@
 // Seleciona o contêiner do SVG
 const svgWrapper = document.getElementById('svg-wrapper');
 // Seleciona o botão de centralizar
-const centralizeButton = document.getElementById('centralize');
+const centralizeButton = document.getElementById('btn-centralize');
 
 // Variáveis para controlar o movimento do SVG
 let isDragging = false;
 let startX, startY, translateX = 0, translateY = 0;
 let scale = 1;
+let maxDraggingX = (svgWrapper.clientWidth / ((scale + 10) / 10)) - 200;
+let maxDraggingY = (svgWrapper.clientHeight / ((scale + 10) / 10)) - 200;
 
 // Adiciona um ouvinte de evento para o clique do botão
 centralizeButton.addEventListener('click', function () {
@@ -14,9 +16,6 @@ centralizeButton.addEventListener('click', function () {
   scale = 1;
   svgWrapper.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`;
 });
-
-let maxDraggingX = (svgWrapper.clientWidth / ((scale + 10) / 10)) - 200;
-let maxDraggingY = (svgWrapper.clientHeight / ((scale + 10) / 10)) - 200;
 
 // Adiciona um ouvinte de evento para pressionar o mouse
 svgWrapper.addEventListener('mousedown', function (event) {
